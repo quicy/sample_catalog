3b073285baea868626ec32b725ba62a066fe5d28
Hideki Kishida <quicy.daemon@gmail.com>
2012-10-23 17:21:40 +0900

    [#38240965] Add image to product

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121023072656) do
+ActiveRecord::Schema.define(:version => 20121023080509) do
 
   create_table "products", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20121023072656) do
     t.datetime "created_at",                 :null => false
     t.datetime "updated_at",                 :null => false
     t.integer  "price",       :default => 0
+    t.string   "image"
   end
 
 end
